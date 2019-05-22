6389e6448e1d84a968e8d9fc7aa9bb3f82f98823
Aidan Skye <aidanskye42@gmail.com>
2019-05-22 13:03:29 -0700

    Scale game to window size and allow resizing

--- src/aura.js
+++ src/aura.js
@@ -6,5 +6,11 @@ document.addEventListener("DOMContentLoaded", ()=>{
   //540p
   canvas.width = WIDTH;
   canvas.height = HEIGHT;
+  const scale = Math.min(window.innerHeight/HEIGHT, window.innerWidth/WIDTH);
+  canvas.style.cssText = `width: ${WIDTH*scale}px; height: ${HEIGHT*scale}px;`;
   new Game(canvas.getContext("2d"));
+  window.addEventListener("resize", () => {
+    const scale = Math.min(window.innerHeight / HEIGHT, window.innerWidth / WIDTH);
+    canvas.style.cssText = `width: ${WIDTH * scale}px; height: ${HEIGHT * scale}px;`;
+  });
 });
